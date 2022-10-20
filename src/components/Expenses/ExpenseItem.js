2ee7714cdf44720c97c2ef3f51e919413f2b13bd
aokamoto <andy.okamoto@gmail.com>
2022-10-19 22:06:51 -0600

    Completed Project

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,18 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import React, { useState } from "react";
+import React from "react";
 
 //props here is passed from the properties in the App.js inline with the ExpenseItem JSX tag
 function ExpenseItem(props) {
   //updating a value from event listener
-  const [title, setTitle] = useState(props.title); //Always returns exactly two elements (value, function to update)
+  // const [title, setTitle] = useState(props.title); //Always returns exactly two elements (value, function to update)
 
   //common practice, end event listener helper method with 'Handler'
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title); //original value doesn't change yet (scheduled for future)
-  };
+  // const clickHandler = () => {
+  //   setTitle("Updated!");
+  //   console.log(title); //original value doesn't change yet (scheduled for future)
+  // };
 
   return (
     <li>
@@ -22,7 +22,7 @@ function ExpenseItem(props) {
           <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
-        <button onClick={clickHandler}>Change Title</button>
+        {/* <button onClick={clickHandler}>Change Title</button> */}
       </Card>
     </li>
   );
